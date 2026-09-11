8fe53e5fa028af6cc86e37103f87451d7099d939
Nino <nino.mandic342@gmail.com>
2026-09-11 21:45:28 +0200

    sezona 3 priprema

--- scripts/sync-index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const src = path.join(__dirname, '..', 'index.html');
-const dest = path.join(__dirname, '..', 'functions', 'public', 'index.html');
-
-fs.copyFileSync(src, dest);
-console.log('Synced index.html -> functions/public/index.html');
